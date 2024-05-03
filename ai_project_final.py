3e51e28d259eaaa79a2abf33eec9d6fbcdcbc3c0
MBT1122 <151543580+MBT1122@users.noreply.github.com>
2024-05-03 17:46:30 +0500

    Update ai_project_final.py

--- ai_project_final.py
+++ ai_project_final.py
@@ -4,7 +4,6 @@ import os
 import zipfile
 import streamlit as st
 from functools import reduce
-from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor
